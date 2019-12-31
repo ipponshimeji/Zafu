74f1e30236689f4f5d6e3f9f657ad94e4cc86d88
Takamasa URAMOTO <ipponshimeji36@live.jp>
2019-12-31 22:14:58 +0900

    Add Utilities project.

---
+++ Src/Utilities/DisposableUtil.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Zafu.Utilities {
+	public static class DisposableUtil {
+		#region methods
+
+		public static void ClearDisposable<T>(ref T? target) where T: class, IDisposable {
+			if (target != null) {
+				IDisposable old = System.Threading.Interlocked.Exchange(ref target, null);
+				old.Dispose();
+			}
+		}
+
+		#endregion
+	}
+}
---
+++ Src/Utilities_test/Test/DisposableUtilTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Zafu.Utilities;
+using Xunit;
+
+namespace Zafu.Utilities.Test {
+	public class DisposableUtilTest {
+		#region types
+
+		public class DisposableSample: IDisposable {
+			#region data
+
+			public int DisposeCount { get; private set; } = 0;
+
+			#endregion
+
+
+			#region IDisposable
+
+			public void Dispose() {
+				++this.DisposeCount;
+			}
+
+			#endregion
+		}
+
+		#endregion
+
+
+		#region ClearDisposable
+
+		public class ClearDisposable {
+			[Fact(DisplayName = "general")]
+			public void General() {
+				// Arrange
+				DisposableSample sample = new DisposableSample();
+				Debug.Assert(sample.DisposeCount == 0);
+				DisposableSample? value = sample;
+
+				// Act
+				DisposableUtil.ClearDisposable(ref value);
+
+				// Assert
+				Assert.Null(value);
+				Assert.Equal(1, sample.DisposeCount);
+			}
+
+			[Fact(DisplayName = "target: null")]
+			public void target_null() {
+				// Arrange
+				DisposableSample? value = null;
+
+				// Act
+				DisposableUtil.ClearDisposable(ref value);
+
+				// Assert
+				Assert.Null(value);
+			}
+		}
+
+		#endregion
+	}
+}
