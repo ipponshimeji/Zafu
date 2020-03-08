d6b252cb08f25ad5b9d669add070996d49f51537
Takamasa URAMOTO <ipponshimeji36@live.jp>
2020-03-08 12:18:08 +0900

    Implement ObjectUtil class.

---
+++ Src/Utilities/ObjectUtil.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Zafu.Utilities {
+	public static class ObjectUtil {
+		#region methods
+
+		public static object? CloneJsonObject(object? src) {
+			// argument checks
+			if (src == null) {
+				// clone null
+				return null;
+			}
+
+			// clone the json object
+			switch (src) {
+				case IDictionary<string, object> obj:
+					// object
+					return obj.ToDictionary(key => key, value => CloneJsonObject(value));
+				case IReadOnlyDictionary<string, object> obj:
+					// object
+					return obj.ToDictionary(key => key, value => CloneJsonObject(value));
+				case IList<object> array:
+					// array
+					return array.Select(child => CloneJsonObject(child)).ToList();
+				case IReadOnlyList<object> array:
+					// array
+					return array.Select(child => CloneJsonObject(child)).ToList();
+				default:
+					// string or value type
+					return src;
+			}
+		}
+
+		#endregion
+	}
+}
---
+++ Src/Utilities_test/Test/ObjectUtilTest.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Zafu.Utilities;
+using Xunit;
+
+namespace Zafu.Utilities.Test {
+	public class ObjectUtilTest {
+		#region CloneJsonObject
+
+		public class CloneJsonObject {
+			[Fact(DisplayName = "src: null")]
+			public void src_null() {
+				// Arrange
+				object? src = null;
+
+				// Act
+				object? actual = ObjectUtil.CloneJsonObject(src);
+
+				// Assert
+				Assert.Null(actual);
+			}
+
+			[Fact(DisplayName = "src: bool")]
+			public void src_bool() {
+				// Arrange
+				bool src = true;
+
+				// Act
+				object? actual = ObjectUtil.CloneJsonObject(src);
+
+				// Assert
+				Assert.Equal(src, actual);
+			}
+
+			[Fact(DisplayName = "src: double")]
+			public void src_double() {
+				// Arrange
+				double src = 123.45;
+
+				// Act
+				object? actual = ObjectUtil.CloneJsonObject(src);
+
+				// Assert
+				Assert.Equal(src, actual);
+			}
+
+			[Fact(DisplayName = "src: string")]
+			public void src_string() {
+				// Arrange
+				string src = "abcde";
+
+				// Act
+				object? actual = ObjectUtil.CloneJsonObject(src);
+
+				// Assert
+				Assert.Equal(src, actual);
+			}
+
+			[Fact(DisplayName = "src: array")]
+			public void src_array() {
+				// Arrange
+				List<string> array = new List<string>() {
+					"a", "b", "c", "d"
+				};
+				IList<string> listSrc = array;
+				IReadOnlyList<string> readOnlyListSrc = array;
+
+				// Act
+				// TODO: imcomplete create wrapper to restrict interfaces
+				object? actual1 = ObjectUtil.CloneJsonObject(listSrc);
+				object? actual2 = ObjectUtil.CloneJsonObject(readOnlyListSrc);
+
+				// Assert
+				Assert.Equal(listSrc, actual1);
+				Assert.NotSame(listSrc, actual1);
+				Assert.Equal(readOnlyListSrc, actual2);
+				Assert.NotSame(readOnlyListSrc, actual2);
+			}
+		}
+
+		// TODO: test object
+
+		#endregion
+	}
+}
