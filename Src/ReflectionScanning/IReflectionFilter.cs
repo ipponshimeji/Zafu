9796be03ed64f843ec474b7c89558f2fefd30485
Takamasa URAMOTO <ipponshimeji36@live.jp>
2020-06-07 14:04:33 +0900

    Add ReflectionScanning

---
+++ Src/ReflectionScanning/IReflectionFilter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Zafu.ReflectionScanning {
+	interface IReflectionFilter {
+		Type[] GetTypes(Assembly assembly);
+		FieldInfo[] GetFields(Type type);
+		PropertyInfo[] GetProperties(Type type);
+		MethodInfo[] GetMethods(Type type);
+	}
+}
---
+++ Src/ReflectionScanning/ReflectionScanner.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace ReflectionScanning {
+	public class ReflectionScanner {
+	}
+}
