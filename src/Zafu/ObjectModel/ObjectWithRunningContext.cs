066c5dc0f3673c5c682ac7fb4ac879f003b34ef1
Takamasa URAMOTO <ipponshimeji36@live.jp>
2020-09-21 13:56:15 +0900

    Add SimpleState<T> pattern loging methods to ObjectWithRunningContext class.

--- src/Zafu/ObjectModel/ObjectWithRunningContext.cs
+++ src/Zafu/ObjectModel/ObjectWithRunningContext.cs
@@ -91,28 +91,52 @@ namespace Zafu.ObjectModel {
 		}
 
 
+		protected void LogTrace(string? message, Exception? exception = null, EventId eventId = default(EventId)) {
+			Log(LogLevel.Trace, message, exception, eventId);
+		}
+
+		protected void LogTrace<T>(string? message, string extraPropName, T extraPropValue, Exception? exception = null, EventId eventId = default(EventId)) {
+			Log<T>(LogLevel.Trace, message, extraPropName, extraPropValue, exception, eventId);
+		}
+
 		protected void LogDebug(string? message, Exception? exception = null, EventId eventId = default(EventId)) {
 			Log(LogLevel.Debug, message, exception, eventId);
 		}
 
-		protected void LogTrace(string? message, Exception? exception = null, EventId eventId = default(EventId)) {
-			Log(LogLevel.Trace, message, exception, eventId);
+		protected void LogDebug<T>(string? message, string extraPropName, T extraPropValue, Exception? exception = null, EventId eventId = default(EventId)) {
+			Log<T>(LogLevel.Debug, message, extraPropName, extraPropValue, exception, eventId);
 		}
 
 		protected void LogInformation(string? message, Exception? exception = null, EventId eventId = default(EventId)) {
 			Log(LogLevel.Information, message, exception, eventId);
 		}
 
+		protected void LogInformation<T>(string? message, string extraPropName, T extraPropValue, Exception? exception = null, EventId eventId = default(EventId)) {
+			Log<T>(LogLevel.Information, message, extraPropName, extraPropValue, exception, eventId);
+		}
+
 		protected void LogWarning(string? message, Exception? exception = null, EventId eventId = default(EventId)) {
 			Log(LogLevel.Warning, message, exception, eventId);
+		}
+
+		protected void LogWarning<T>(string? message, string extraPropName, T extraPropValue, Exception? exception = null, EventId eventId = default(EventId)) {
+			Log<T>(LogLevel.Warning, message, extraPropName, extraPropValue, exception, eventId);
 		}
 
 		protected void LogError(string? message, Exception? exception = null, EventId eventId = default(EventId)) {
 			Log(LogLevel.Error, message, exception, eventId);
 		}
 
+		protected void LogError<T>(string? message, string extraPropName, T extraPropValue, Exception? exception = null, EventId eventId = default(EventId)) {
+			Log<T>(LogLevel.Error, message, extraPropName, extraPropValue, exception, eventId);
+		}
+
 		protected void LogCritical(string? message, Exception? exception = null, EventId eventId = default(EventId)) {
 			Log(LogLevel.Critical, message, exception, eventId);
+		}
+
+		protected void LogCritical<T>(string? message, string extraPropName, T extraPropValue, Exception? exception = null, EventId eventId = default(EventId)) {
+			Log<T>(LogLevel.Critical, message, extraPropName, extraPropValue, exception, eventId);
 		}
 
 		#endregion
@@ -132,6 +156,10 @@ namespace Zafu.ObjectModel {
 
 		protected virtual void Log(LogLevel logLevel, string? message, Exception? exception, EventId eventId) {
 			LoggingUtil.Log(this.Logger, logLevel, this.NameForLogging, message, exception, eventId);
+		}
+
+		protected virtual void Log<T>(LogLevel logLevel, string? message, string extraPropName, T extraPropValue, Exception? exception, EventId eventId) {
+			LoggingUtil.Log<T>(this.Logger, logLevel, this.NameForLogging, message, extraPropName, extraPropValue, exception, eventId);
 		}
 
 		#endregion
